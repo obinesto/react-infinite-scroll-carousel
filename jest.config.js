1acd81a83af44634cdde642dfff650130e04ee07
Cyprian Obi <obicyprian180@gmail.com>
2025-08-18 17:03:59 +0100

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,23 @@
-export default {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.mjs'],
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js'
-  },
+// export default {
+//   testEnvironment: 'jsdom',
+//   setupFilesAfterEnv: ['<rootDir>/jest.setup.mjs'],
+//   moduleNameMapper: {
+//     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+//     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js'
+//   },
+//   transform: {
+//     '^.+\\.(ts|tsx)$': 'ts-jest'
+//   },
+//   extensionsToTreatAsEsm: ['.ts', '.tsx'],
+//   globals: {
+//     'ts-jest': { useESM: true }
+//   }
+// };
+
+export default{
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest'
+    '^.+\\.(ts|tsx)$': 'babel-jest'
   },
-  extensionsToTreatAsEsm: ['.ts', '.tsx'],
-  globals: {
-    'ts-jest': { useESM: true }
-  }
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 };
