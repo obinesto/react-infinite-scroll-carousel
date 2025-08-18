342634cf7874cd860d68ea215ae573d326f1313d
Cyprian Obi <obicyprian180@gmail.com>
2025-08-18 15:55:16 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
-export const testEnvironment = 'jsdom';
-export const setupFilesAfterEnv = ['<rootDir>/jest.setup.js'];
-export const moduleNameMapper = {
-  '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-  '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js'
-};
-export const transform = {
-  '^.+\\.(ts|tsx)$': 'ts-jest'
+export default {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js'
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  }
 };
