86ff78082f71749357bbf77200b553581e156910
Cyprian Obi <obicyprian180@gmail.com>
2025-08-18 19:43:22 +0100

    update jest

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
-module.exports = {
+export default {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-typescript',
-    '@babel/preset-react'
-  ]
+    "@babel/preset-env",
+    "@babel/preset-typescript",
+    "@babel/preset-react",
+  ],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,16 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.mjs"],
+/** @type {import('jest').Config} */
+
+const config = {
+  testEnvironment: "jest-environment-jsdom",
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__mocks__/fileMock.js",
   },
   transform: {
-    "^.+\\.(ts|tsx)$": "babel-jest",
+    "\\.[jt]sx?$": "babel-jest",
   },
 };
+
+export default config;
